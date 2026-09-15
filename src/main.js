13c5d78d6e057b100a285f8faa7550852689752f
93kumi <kumi.rura.k9780@gmail.com>
2026-09-15 21:47:57 +0900

    Update counter app

--- src/main.js
+++ src/main.js
@@ -3,15 +3,30 @@ import './style.css';
 document.querySelector('#app').innerHTML = `
   <h1>カウンター</h1>
   <p id="count">0</p>
-  <button id="btn">増やす</button>
+
+  <button id="plus">増やす</button>
+  <button id="minus">減らす</button>
+  <button id="reset">リセット</button>
 `;
 
 const countEl = document.querySelector('#count');
-const btn = document.querySelector('#btn');
+const plusBtn = document.querySelector('#plus');
+const minusBtn = document.querySelector('#minus');
+const resetBtn = document.querySelector('#reset');
+
 let count = 0;
 
-btn.addEventListener('click', () => {
+plusBtn.addEventListener('click', () => {
   count += 1;
   countEl.textContent = count;
 });
 
+minusBtn.addEventListener('click', () => {
+  count -= 1;
+  countEl.textContent = count;
+});
+
+resetBtn.addEventListener('click', () => {
+  count = 0;
+  countEl.textContent = count;
+});
